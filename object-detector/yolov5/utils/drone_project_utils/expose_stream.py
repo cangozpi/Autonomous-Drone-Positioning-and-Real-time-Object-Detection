38292e872c65095377c2c59688c48dc324b253fe
can <can@can-ubuntu-18.04>
2021-12-06 00:31:59 +0300

    removed yolov5 submodule and added them normally. Sample website for live video-straming created under utils/drone_utils using Flask

---
+++ object-detector/yolov5/utils/drone_project_utils/expose_stream.py
@@ -0,0 +1,35 @@
+#Import necessary libraries
+from flask import Flask, render_template, Response
+import cv2
+#Initialize the Flask app
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
+for local webcam use cv2.VideoCapture(0)
+'''
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
