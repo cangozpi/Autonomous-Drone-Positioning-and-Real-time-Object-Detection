c92aa1acd7c2264459dc3cb21047fea4908b18b4
can <can@can-ubuntu-18.04>
2021-12-06 02:57:09 +0300

    website live stream concurrency is handled with acceptable fps. Uses opencv camera should switch to using yolo model in the future.

--- object-detector/yolov5/utils/drone_project_utils/expose_stream.py
+++ object-detector/yolov5/utils/drone_project_utils/expose_stream.py
@@ -2,6 +2,24 @@ from flask import Flask, render_template, Response
 import cv2
 import threading
 
+# -- import detect.py from upper directory
+import sys
+from pathlib import Path
+sys.path.append(str(Path('.').absolute().parent.parent))
+print(str(Path('.').absolute().parent.parent))
+
+# Now you can import your module 
+from yolov5.detect import *
+
+#from ...detect import *
+
+#import sys
+# setting path
+#sys.path.append('../../yolov5')
+  
+# importing
+#from yolov5.detect import *
+# --
 
 outputFrame = None
 lock = threading.Lock()
@@ -9,11 +27,8 @@ lock = threading.Lock()
 #Initialize the Flask app
 app = Flask(__name__)
 
-#camera = cv2.VideoCapture(0)
-from imutils.video import VideoStream
-vs = VideoStream(src=0).start()
-import time
-time.sleep(2.0)
+camera = cv2.VideoCapture(0)
+
 '''
 for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
 for local webcam use cv2.VideoCapture(0)
@@ -22,7 +37,7 @@ for local webcam use cv2.VideoCapture(0)
 def gen_frames():  
     # grab global references to the output frame and lock variables
     global outputFrame, lock
-        # loop over frames from the output stream
+    # loop over frames from the output stream
     while True:
         # wait until the lock is acquired
         with lock:
@@ -35,31 +50,43 @@ def gen_frames():
             if not flag:
                 continue
             encodedImage = encodedImage.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + encodedImage + b'\r\n')
+        yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + encodedImage + b'\r\n')
 
    
         
 
-def detect():
+"""def detect():
 	# grab global references to the video stream, output frame, and
 	# lock variables
-    global camera, outputFrame, lock, vs
+    global camera, outputFrame, lock
 	
     # loop over frames from the video stream
     while True:
 		# read the next frame from the video stream, resize it,
 		# convert the frame to grayscale, and blur it
-        success = True
-        frame =vs.read()
-        #success, frame = camera.read()
+        success, frame = camera.read()
         if not success:
             break
         else:
             # acquire the lock, set the output frame, and release the
             # lock
             with lock:
-                outputFrame = frame.copy()
+                outputFrame = frame.copy()"""
+
+
+## test start--
+
+def detect(opt):
+	# grab global references to the video stream, output frame, and
+	# lock variables
+    main(opt)
+            # acquire the lock, set the output frame, and release the
+            # lock
+            #with lock:
+            #    outputFrame = frame.copy()
+
+## test end--
 
 
 @app.route('/')
@@ -72,8 +99,13 @@ def video_feed():
 
 
 if __name__ == '__main__':
-	t = threading.Thread(target=detect)
-	t.daemon = True
-	t.start()
+    # parse the parameters for object detector
+    opt = parse_opt()
+
+    # start to live stream
+    t = threading.Thread(target=detect, args=(
+		opt,))
+    t.daemon = True
+    t.start()
 	# start the flask app
-	app.run(debug=False, threaded=True, use_reloader=False)
+    app.run(debug=False, threaded=True, use_reloader=False)
